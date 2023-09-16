cbd8f2af66feadbe0cce631240f502469a29360a
SagarPradhan613 <karanpradhan42@gmail.com>
2023-09-16 01:57:21 -0700

    Initial commit

---
+++ frontend/task-manager-frontend/src/redux/Environment/ApiEnvironment.js
@@ -0,0 +1,7 @@
+// ApiEnvironment.js
+
+// API URL for tasks
+// const API_URL = "http://192.168.1.4:3000/api/tasks";
+const API_URL = "https://deploybackend-two.vercel.app/api/tasks";
+
+export default API_URL;
